4968bdd722b1b8696e485267cce4895675ebb143
Tomasz QA <idzikqa@gmail.com>
2020-09-30 11:27:06 +0200

    added possibility to use js

--- src/test/java/pl/idzikqa/herokuapp/hooks/BasePage.java
+++ src/test/java/pl/idzikqa/herokuapp/hooks/BasePage.java
@@ -1,15 +1,18 @@
 package pl.idzikqa.herokuapp.hooks;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import pl.idzikqa.herokuapp.generics.GenericMethods;
 
 public abstract class BasePage {
     protected WebDriver driver;
+    protected JavascriptExecutor js;
     protected GenericMethods gm;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        this.js=(JavascriptExecutor) driver;
         this.gm=new GenericMethods(driver);
         PageFactory.initElements(driver,this);
     }
