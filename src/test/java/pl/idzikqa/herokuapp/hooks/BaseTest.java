4e1ffa80f45da174f3b5c55aed50cd429ad0df0b
Idzik QA <idzikqa@gmail.com>
2020-10-04 00:12:47 +0200

    added test suite with parameters

--- src/test/java/pl/idzikqa/herokuapp/hooks/BaseTest.java
+++ src/test/java/pl/idzikqa/herokuapp/hooks/BaseTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.*;
 import pl.idzikqa.herokuapp.pages.AddRemoveElementsPage;
 import pl.idzikqa.herokuapp.pages.MainPage;
@@ -19,42 +21,52 @@ public abstract class BaseTest {
     protected DragAndDropPage dragAndDropPage;
     protected HoversPage hoversPage;
 
-    @BeforeClass
-    public void beforeClass() {
-        System.out.println("Before class");
-    }
-
-    @AfterClass
-    public void afterClass() {
-        System.out.println("After class");
-    }
-
-    @BeforeSuite
-    public void beforeSuite() {
-        System.out.println("Before suite");
-    }
-
-    @AfterSuite
-    public void afterSuite() {
-        System.out.println("After suite");
-    }
-
-    @BeforeTest
-    public void beforeTest() {
-        System.out.println("Before test in suite");
-    }
-
-    @AfterTest
-    public void afterTest(){
-        System.out.println("After test in suite");
-    }
+//    @BeforeClass
+//    public void beforeClass() {
+//        System.out.println("Before class");
+//    }
+//
+//    @AfterClass
+//    public void afterClass() {
+//        System.out.println("After class");
+//    }
+//
+//    @BeforeSuite
+//    public void beforeSuite() {
+//        System.out.println("Before suite");
+//    }
+//
+//    @AfterSuite
+//    public void afterSuite() {
+//        System.out.println("After suite");
+//    }
+//
+//    @BeforeTest
+//    public void beforeTest() {
+//        System.out.println("Before test in suite");
+//    }
+//
+//    @AfterTest
+//    public void afterTest(){
+//        System.out.println("After test in suite");
+//    }
 
     @BeforeMethod(alwaysRun = true)
-    public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+    @Parameters({"browser"})
+    public void setup(@Optional("chrome") String browser) {
+        if (browser.equals("chrome")){
+            ChromeOptions options = new ChromeOptions();
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+        }
+        else {
+            FirefoxOptions options = new FirefoxOptions();
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver(options);
+            driver.manage().window().maximize();
+        }
+
         driver.get(site);
         mainPage = new MainPage(driver);
         addRemoveElementsPage = new AddRemoveElementsPage(driver);
