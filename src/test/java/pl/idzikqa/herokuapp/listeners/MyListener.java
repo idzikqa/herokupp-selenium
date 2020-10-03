b314151a80acadaa83f3650faaba6d1955221ca8
Idzik QA <idzikqa@gmail.com>
2020-10-04 01:28:57 +0200

    added custom InvokeMethodListener

---
+++ src/test/java/pl/idzikqa/herokuapp/listeners/MyListener.java
@@ -0,0 +1,23 @@
+package pl.idzikqa.herokuapp.listeners;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+public class MyListener implements IInvokedMethodListener {
+    @Override
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        System.out.println(
+                "Before Invocation Method Started For : " + method.getTestMethod().getMethodName()
+                        + " of Class :" + testResult.getTestClass().getName());
+    }
+
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+
+        System.out.println("After Invocation Method Started For : "
+                + method.getTestMethod().getMethodName()
+                + " of Class : " + testResult.getTestClass().getName());
+
+    }
+}
--- src/test/java/pl/idzikqa/herokuapp/testcases/HoversTest.java
+++ src/test/java/pl/idzikqa/herokuapp/testcases/HoversTest.java
@@ -1,9 +1,13 @@
 package pl.idzikqa.herokuapp.testcases;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pl.idzikqa.herokuapp.hooks.BaseTest;
+import pl.idzikqa.herokuapp.listeners.MyListener;
 
+@Listeners(value = MyListener.class)
 public class HoversTest extends BaseTest {
+
     @Test(groups = {"smoke"})
     public void HoverTest(){
 
