f8d0878f5d2997033e1fc28c30352acf3decaf23
Idzik QA <idzikqa@gmail.com>
2020-10-04 10:01:12 +0200

    refactor of listeners

--- src/test/java/pl/idzikqa/herokuapp/listeners/MyInvokeMethodListener.java
+++ src/test/java/pl/idzikqa/herokuapp/listeners/MyInvokeMethodListener.java
@@ -7,6 +7,7 @@ import org.testng.ITestResult;
 public class MyInvokeMethodListener implements IInvokedMethodListener {
     @Override
     public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        //before every method in test class
         System.out.println(
                 "Before Invocation Method Started For : " + method.getTestMethod().getMethodName()
                         + " of Class :" + testResult.getTestClass().getName());
@@ -14,7 +15,7 @@ public class MyInvokeMethodListener implements IInvokedMethodListener {
 
     @Override
     public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
-
+        //after every method on test class
         System.out.println("After Invocation Method Started For : "
                 + method.getTestMethod().getMethodName()
                 + " of Class : " + testResult.getTestClass().getName());
--- src/test/java/pl/idzikqa/herokuapp/listeners/MyTestListener.java
+++ src/test/java/pl/idzikqa/herokuapp/listeners/MyTestListener.java
@@ -2,41 +2,55 @@ package pl.idzikqa.herokuapp.listeners;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 
 public class MyTestListener implements ITestListener {
     @Override
     public void onStart(ITestContext context) {
-        System.out.println("onStart method started");
+        // before <test> tag in xml file
+        System.out.println("ITestListener : onStart method started");
+        ITestResult result;
+        ITestNGMethod methods[] = context.getAllTestMethods();
+        System.out.println("Methods will be executed in this test tag");
+        for (ITestNGMethod method : methods
+        ) {
+            System.out.println(method.getMethodName());
+        }
     }
 
     @Override
     public void onFinish(ITestContext context) {
-        System.out.println("onFinish method started");
+        // after <test> tag in xml file
+        System.out.println("ITestListener : onFinish method started");
     }
 
     @Override
     public void onTestStart(ITestResult result) {
-        System.out.println("New Test Started" + result.getName());
+        //when test method starts
+        System.out.println("ITestListener : New Test Started" + result.getName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println("onTestSuccess Method" + result.getName());
+        //if test method is successful
+        System.out.println("ITestListener : onTestSuccess Method" + result.getName());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("onTestFailure Method" + result.getName());
+        //if test method is failed
+        System.out.println("ITestListener : onTestFailure Method" + result.getName());
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        System.out.println("onTestSkipped Method" + result.getName());
+        //if test method is skipped
+        System.out.println("ITestListener : onTestSkipped Method" + result.getName());
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        System.out.println("onTestFailedButWithinSuccessPercentage" + result.getName());
+        System.out.println("ITestListener : onTestFailedButWithinSuccessPercentage" + result.getName());
     }
 }
